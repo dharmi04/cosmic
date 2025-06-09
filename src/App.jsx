0768d7fa06b51587c37ca15df856d52c78a00f2a
dharmi04 <dharmip060@gmail.com>
2025-06-09 13:21:28 +0530

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,8 @@ import MouseLightEffect from "./components/MouseLightEffect";
 function App() {
   return (
     <div style={{ position: 'relative', minHeight: '100vh' }}>
-      {/* Global Mouse Light Effect */}
       <MouseLightEffect />
-      
-      {/* Your existing components */}
-      <BirthOptions />
+            <BirthOptions />
       <SceneCanvas />
       <ImageRotation3D />
       <FlowingTextAnimation />
